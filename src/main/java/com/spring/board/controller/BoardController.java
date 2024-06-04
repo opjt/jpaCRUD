d429948fda87fdf657260d1bc1072588422f53cc
opjt <jtpark1957@gmail.com>
2024-06-04 21:47:01 +0900

    검색 내림차순 정렬, 페이지네이션 버그 수정

--- src/main/java/com/spring/board/controller/BoardController.java
+++ src/main/java/com/spring/board/controller/BoardController.java
@@ -28,13 +28,17 @@ public class BoardController {
 
     private final BoardService boardService;
 
+
+
+
     @GetMapping("/board/list")
     public String listBoards(PageDTO pageDTO, Model model) throws Exception {
         Pageable pageable = PageRequest.of(pageDTO.getPage()-1, pageDTO.getSize());
         Page<Board> boardPage = boardService.boardList(pageDTO.getKeyword(), pageable);
-        pageDTO.setTotalPages(boardPage.getTotalPages());
+
+        pageDTO.setTotalPages(boardPage.getTotalPages() == 0 ? 1 : boardPage.getTotalPages());
         pageDTO.setStartPage(Math.max(1, pageDTO.getPage() - 2));
-        pageDTO.setEndPage(Math.min(boardPage.getTotalPages(), pageDTO.getPage() + 2));
+        pageDTO.setEndPage(Math.min(pageDTO.getTotalPages(), pageDTO.getPage() + 2));
         model.addAttribute("boardPage", boardPage);
         model.addAttribute("page", pageDTO);
         return "board/list";
@@ -48,6 +52,7 @@ public class BoardController {
 
     @PostMapping("/board/")
     public String saveBoard(@ModelAttribute("board") BoardDTO board) throws Exception {
+        System.out.println(board.toString());
         Board updateBoard = boardService.writeBoard(board);
         return "redirect:/board/list";
     }
@@ -59,7 +64,6 @@ public class BoardController {
             String errorMessage = URLEncoder.encode("찾을 수 없는 글입니다", "UTF-8");
             return "redirect:/errorPage?errorMessage=" + errorMessage;
         }
-        log.info(board.getComments().toString());
         model.addAttribute("board", board);
         return "board/read";
     }
@@ -80,7 +84,7 @@ public class BoardController {
         boardDto.setId(id); // 기존 게시물의 ID를 설정하여 업데이트를 수행
         log.info(boardDto.toString());
         Board board = boardService.updateBoard(boardDto);
-        return "redirect:/board/" + id;
+        return "redirect:/board/" + board.getId();
     }
 
     @PostMapping("/board/delete/{id}")
